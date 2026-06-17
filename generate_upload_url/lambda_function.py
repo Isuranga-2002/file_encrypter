1be48a396b041f6daa4d1e9197d791de0eaa93f9
Isuranga-2002 <166277109+Isuranga-2002@users.noreply.github.com>
2026-06-17 18:17:24 +0530

    feat: replace test lambda with real URL generation logic

--- generate_upload_url/lambda_function.py
+++ generate_upload_url/lambda_function.py
@@ -1,10 +1,40 @@
 import json
+import boto3
+import uuid
+
+s3_client = boto3.client('s3')
+
+SOURCE_BUCKET = 'isuranga-source-bucket-file-encrypter'
 
 def lambda_handler(event, context):
 
+    body = json.loads(event.get('body', '{}'))
+
+    filename = body.get('filename')
+
+    if not filename:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({
+                'message': 'filename is required'
+            })
+        }
+
+    file_key = f"uploads/{uuid.uuid4()}-{filename}"
+
+    upload_url = s3_client.generate_presigned_url(
+        'put_object',
+        Params={
+            'Bucket': SOURCE_BUCKET,
+            'Key': file_key
+        },
+        ExpiresIn=300
+    )
+
     return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "Upload URL endpoint works"
+        'statusCode': 200,
+        'body': json.dumps({
+            'uploadUrl': upload_url,
+            'fileKey': file_key
         })
     }
